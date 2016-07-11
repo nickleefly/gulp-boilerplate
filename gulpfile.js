2307ef5c778e0aa3ebf2218ab1f3a38ebfe8e0c9
Xiuyu Li <nickleefly@gmail.com>
2016-07-11 14:23:58 +0800

    using less

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 var gulp = require('gulp'),
-  sass = require('gulp-sass'),
+  less = require('gulp-less'),
   sourcemaps = require('gulp-sourcemaps'),
   autoprefixer = require('gulp-autoprefixer'),
   changed = require('gulp-changed'),
@@ -13,24 +13,22 @@ var gulp = require('gulp'),
   rename = require('gulp-rename'),
   del = require('del'),
   vinylPaths = require('vinyl-paths'),
-  iconfont = require('gulp-iconfont'),
-  swig = require('gulp-swig'),
   factor = require('factor-bundle'),
   browserSync = require('browser-sync').create(),
-  reactify = require('reactify');
+  reactify = require('reactify')
 
-var dest = "./web/static",
+var dest = './web/static',
   config = {
-    sass: {
-      src: "./web/css/**/*.{sass,scss}",
+    less: {
+      src: './web/css/**/*.less',
       dest: dest
     },
     images: {
-      src: "./web/images/**",
-      dest: dest + "/images"
+      src: './web/images/**',
+      dest: dest + '/images'
     },
     javascript: {
-      src: "./web/javascript",
+      src: './web/javascript',
       dest: dest
     },
     production: {
@@ -38,78 +36,48 @@ var dest = "./web/static",
       jsSrc: dest + '/*.js',
       dest: dest
     },
-    iconfont: {
-      src: './web/icons/*.svg',
-      dest: dest + '/fonts',
-      sassDest: './web/css',
-      template: './web/css/template/template.sass.swig',
-      sassOutputName: '_icons.sass',
-      fontPath: '../static/fonts',
-      className: 'icon',
-      options: {
-        fontName: 'Boilerplate',
-        appendCodepoints: true,
-        normalize: false
-      }
+    font: {
+      src: './web/fonts',
+      dest: dest
     }
-  };
-
-function generateIconSass(codepoints, options) {
-  gulp.src(config.iconfont.template)
-    .pipe(swig({
-      data: {
-        icons: codepoints.map(function(icon) {
-          return {
-            name: icon.name,
-            code: icon.codepoint.toString(16)
-          }
-        }),
-        fontName: config.iconfont.options.fontName,
-        fontPath: config.iconfont.fontPath,
-        className: config.iconfont.className,
-      }
-    }))
-    .pipe(rename(config.iconfont.sassOutputName))
-    .pipe(gulp.dest(config.iconfont.sassDest));
 }
+gulp.task('copy', function () {
+  return gulp.src('./node_modules/bootstrap/fonts/glyphicons-halflings-regular.{eot,svg,ttf,woff,woff2}')
+    .pipe(gulp.dest(config.font.dest + '/fonts/'))
+})
 
-gulp.task('iconfont', function() {
-  return gulp.src(config.iconfont.src)
-    .pipe(iconfont(config.iconfont.options))
-    .on('codepoints', generateIconSass)
-    .pipe(gulp.dest(config.iconfont.dest));
-});
+var LessPluginAutoPrefix = require('less-plugin-autoprefix'),
+  autoprefix = new LessPluginAutoPrefix({browsers: ['last 2 versions']})
 
-gulp.task('sass', ['iconfont'], function () {
-  return gulp.src(config.sass.src)
-    // .pipe(sourcemaps.init())
-    .pipe(sass(config.sass.settings))
-    //.pipe(sourcemaps.write())
-    .pipe(autoprefixer({ browsers: ['last 2 version'] }))
-    .pipe(gulp.dest(config.sass.dest))
-    .pipe(browserSync.reload({stream: true}));
-});
+gulp.task('less', ['copy'], function () {
+  return gulp.src(config.less.src)
+    .pipe(less({
+      plugins: [autoprefix]
+    }))
+    .pipe(gulp.dest(config.less.dest))
+    .pipe(browserSync.reload({stream: true}))
+})
 
 gulp.task('clean:css', function () {
   return gulp.src(dest + '/index.min.css')
-    .pipe(vinylPaths(del));
-});
+    .pipe(vinylPaths(del))
+})
 
-gulp.task('minify', ['sass', 'clean:css'], function() {
+gulp.task('minify', ['less', 'clean:css'], function () {
   return gulp.src(config.production.cssSrc)
     .pipe(rename('index.min.css'))
-    .pipe(minifyCSS({keepBreaks:true}))
+    .pipe(minifyCSS({keepBreaks: true}))
     .pipe(gulp.dest(config.production.dest))
-    .pipe(size());
-});
+    .pipe(size())
+})
 
-gulp.task('images', function() {
+gulp.task('images', function () {
   return gulp.src(config.images.src)
     .pipe(changed(config.images.dest)) // Ignore unchanged files
     .pipe(imagemin()) // Optimize
     .pipe(gulp.dest(config.images.dest))
-    .pipe(browserSync.reload({stream: true}));
-});
+    .pipe(browserSync.reload({stream: true}))
+})
 
 gulp.task('browserify', function () {
   return browserify({
@@ -122,51 +90,32 @@ gulp.task('browserify', function () {
     debug: true, // Gives us sourcemapping
     cache: {}, packageCache: {}, fullPaths: true
   })
-  .plugin(factor, { o: [
-    config.javascript.dest + '/index.js',
-    config.javascript.dest + '/featureA.js',
-    config.javascript.dest + '/featureB.js'
-    ]
-  })
-  .bundle() // Create the initial bundle when starting the task
-  .pipe(source('common.js'))
-  .pipe(gulp.dest(config.javascript.dest))
-  .pipe(browserSync.reload({stream: true}));
-
-  // return browserify([config.javascript.src + '/index.js']).bundle()
-  //   .pipe(source('index.js'))
-  //   .pipe(buffer())
-  //   .pipe(gulp.dest(config.javascript.dest)).pipe(browserSync.reload({
-  //     stream: true
-  //   }));
-
-  // var files = [
-  //   config.javascript.src + '/index.js',
-  //   config.javascript.src + '/featureB.js'
-  // ];
-  // return  browserify(files)
-  // .plugin(factor, { o: [
-  //   config.javascript.dest + '/index.js',
-  //   config.javascript.dest + '/featureB.js'
-  //   ]
-  // })
-  // .bundle().pipe(fs.createWriteStream( config.javascript.dest + '/common.js'));
-});
+    .plugin(factor, { o: [
+        config.javascript.dest + '/index.js',
+        config.javascript.dest + '/featureA.js',
+        config.javascript.dest + '/featureB.js'
+      ]
+    })
+    .bundle() // Create the initial bundle when starting the task
+    .pipe(source('common.js'))
+    .pipe(gulp.dest(config.javascript.dest))
+    .pipe(browserSync.reload({stream: true}))
+})
 
 gulp.task('clean:js', function () {
   return gulp.src(dest + '/*.min.js')
-    .pipe(vinylPaths(del));
-});
+    .pipe(vinylPaths(del))
+})
 
-gulp.task('uglify', ['browserify', 'clean:js'], function() {
+gulp.task('uglify', ['browserify', 'clean:js'], function () {
   return gulp.src(config.production.jsSrc)
     .pipe(rename({suffix: '.min'}))
     .pipe(uglify())
     .pipe(gulp.dest(config.production.dest))
-    .pipe(size());
-});
+    .pipe(size())
+})
 
-gulp.task('browser-sync', function() {
+gulp.task('browser-sync', function () {
   browserSync.init({
     files: ['web/static/*.css', 'web/static/*.js'],
     // server: {
@@ -176,23 +125,23 @@ gulp.task('browser-sync', function() {
     // proxy: "proxyurl",
     // port: 8000
     proxy: {
-      target: "http://127.0.0.1:8080",
+      target: 'http://127.0.0.1:8080',
       reqHeaders: function (config) {
         return {
-          "host": config.urlObj.host,
-          "accept-encoding": "identity",
-          "agent": false
+          'host': config.urlObj.host,
+          'accept-encoding': 'identity',
+          'agent': false
         }
       }
     }
-  });
-});
+  })
+})
 
-gulp.task('watch', ['browser-sync'], function(callback) {
-  gulp.watch(config.sass.src, ['sass'], browserSync.reload);
-  gulp.watch(config.images.src, ['images'], browserSync.reload);
-  gulp.watch(config.javascript.src + '/**/*.js', ['browserify'], browserSync.reload);
-});
+gulp.task('watch', ['browser-sync'], function (callback) {
+  gulp.watch(config.less.src, ['less'], browserSync.reload)
+  gulp.watch(config.images.src, ['images'], browserSync.reload)
+  gulp.watch(config.javascript.src + '/**/*.js', ['browserify'], browserSync.reload)
+})
 
-gulp.task('default', ['sass', 'browserify', 'images']);
-gulp.task('prod', ['minify', 'images', 'uglify']);
+gulp.task('default', ['less', 'browserify', 'images'])
+gulp.task('prod', ['minify', 'images', 'uglify'])
